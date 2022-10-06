f5be3de553d981c26404f6ce7df352ed208002a7
MoFares7 <faresdabbas1@gmail.com>
2022-10-06 13:03:27 +0300

    add css

--- hetod/src/components/body.js
+++ hetod/src/components/body.js
@@ -2,9 +2,7 @@ import React from "react";
 
 function Body() {
     return (
-        <p style={{
-            color:'red',
-        }}> this is my account in social medial </p>
+        <p> this is my account in social medial </p>
 
     );
 }
