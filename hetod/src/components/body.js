b8dce371a48d2836905e768d2bd082871550ab21
MoFares7 <faresdabbas1@gmail.com>
2022-10-07 14:09:32 +0300

    solve homework 1

--- hetod/src/components/body.js
+++ hetod/src/components/body.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 function Body() {
+
+    let choise = 'designer';
+    let title = {
+        designer: 'designer',
+        programmer: 'programmer'
+    }
+   
     return (
-        <p> this is my account in social medial </p>
+        <p> this is my account use <h5 style={{ ...choise == 'designer' ? 'color: blue;' : 'color: red;' }}> {title.designer}</h5> or <h5>{title.programmer}</h5> it is up to client </p>
 
     );
 }
