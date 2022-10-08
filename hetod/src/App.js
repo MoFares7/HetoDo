3c6ad52eb6b2fa9b39baa4379c75610cd96f10f7
MoFares7 <faresdabbas1@gmail.com>
2022-10-08 13:37:42 +0300

    add state

--- hetod/src/App.js
+++ hetod/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import './App.css';
+import './css/App.css';
 import Hetodo from './components/Hetodo';
 
 class App extends Component {
-  render() {
-    return (
-   <Hetodo />
-    );
+     render() {
+         return (
+            <Hetodo />
+      );
+    }
   }
-}
 export default App;
--- hetod/src/components/Team/OurTeamItems.js
+++ hetod/src/components/Team/OurTeamItems.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
-function OurTeamItem(props) {
+class OurTeamItem extends Component() {
 
-    return (
-    <div className="col-md-4 col-sm-6">    
-        <div className="card">
-            <div className="card-header">
-                <img style={{maxWidth: '100%'}} src={props.image}></img>
+    render() {
+        return (
+            <div className="col-md-4 col-sm-6">
+                <div className="card">
+                    <div className="card-header">
+                        <img style={{ maxWidth: '100%' }} src={this.props.image}></img>
+                    </div>
+                    <div className="card-body" style={{
+                        background: this.props.website
+                            ? 'white' : 'green'
+                    }}>
+                        <h3>{this.props.name}</h3>
+                        <p className="work">{this.props.work}</p>
+                        <p className="Phone">{this.props.phone} </p>
+                        <p className="Email">{this.props.email} </p>
+                        <p className="Website">{this.props.website} </p>
+                    </div>
+                </div>
             </div>
-                <div className="card-body" style={{
-                    background: props.website
-                        ? 'white' : 'green'
-                }}>
-            <h3>{props.name}</h3>
-            <p className="work">{props.work}</p>
-            <p className="Phone">{props.phone} </p>
-            <p className="Email">{props.email} </p>
-            <p className="Website">{props.website} </p>
-            </div>
-        </div>
-    </div>
-    );
+        );
+    }
 }
 
 export default OurTeamItem;
