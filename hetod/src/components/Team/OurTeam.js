233e10839361916e788924117065654d09404442
MoFares7 <faresdabbas1@gmail.com>
2022-10-09 17:46:06 +0300

    add statemanagment

--- hetod/src/components/Team/OurTeam.js
+++ hetod/src/components/Team/OurTeam.js
@@ -10,30 +10,42 @@ class Team extends Component {
         this.state = {
             memberInfo: memberInfo,
             members: [],
+            index: 0,
         }
         this.clickEvent = this.clickEvent.bind(this)
     }
 
     clickEvent() {
-     
-        this.state.members.push(this.state.memberInfo[1])
-        this.newMembers = this.state.members.map(
-            member =>
-                <OurTeamItem
-                    key={member.id}
-                    name={member.name}
-                    image={member.image}
-                    phone={member.phone}
-                    email={member.email}
-                    website={member.website}
-                />);
+
+        if (this.state.index < this.state.memberInfo.length) {
+            this.state.members.push(this.state.memberInfo[this.state.index]);
+            this.setState(
+                function (lastIndex) {
+                    return {
+                        index: lastIndex.index + 1
+                    }
+                }
+            );
+            console.log(this.state.members);
+            this.newMembers = this.state.members.map(
+                member =>
+                    <OurTeamItem
+                        key={member.id}
+                        name={member.name}
+                        image={member.image}
+                        phone={member.phone}
+                        email={member.email}
+                        website={member.website}
+                    />);
+        }
+
     }
 
     render() {
         return (
             <div className="row ">
                 <button className="btn btn-primary btn-lg btn-block" onClick={this.clickEvent}>Add Employee</button>
-                {this.members}
+                {this.newMembers}
             </div>
         )
     }
